c0ff638a2411564292745029e7d8310f88b84671
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2021-01-09 17:41:06 +0000

    [LeetCode Sync] Runtime - 84 ms (17.81%), Memory - 14.1 MB (100.00%)

---
+++ my-folder/0777-toeplitz-matrix/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        
+        for row in range(1,len(matrix)):
+            for col in range(1,len(matrix[row])):
+                if matrix[row-1][col-1] !=  matrix[row][col]:
+                    return False
+        
+        return True
