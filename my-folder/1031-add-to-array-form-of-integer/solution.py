904085fc629280066617e0f76cd471a842be3671
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2021-02-09 05:37:51 +0000

    [LeetCode Sync] Runtime - 296 ms (26.78%), Memory - 14.7 MB (100.00%)

--- my-folder/1031-add-to-array-form-of-integer/solution.py
+++ my-folder/1031-add-to-array-form-of-integer/solution.py
@@ -1,4 +1,16 @@
 class Solution:
     def addToArrayForm(self, A: List[int], K: int) -> List[int]:
-        # a = int(''.join(A))
-        return [i for i in str(int(''.join([str(j) for j in A]))+K)]
+        
+        s = ''
+        for j in A:
+            s += str(j)
+        
+        ar = []
+        Sum = str(int(s) + K)
+        for i in Sum:
+            ar.append(i)
+            
+        return ar
+        
+        
+        # return [i for i in str(int(''.join([str(j) for j in A]))+K)]
