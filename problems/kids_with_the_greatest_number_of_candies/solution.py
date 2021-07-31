c8efae8283c3ffdeacb88c4925421093d8e67b3d
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2021-07-31 17:28:53 +0000

    Sync LeetCode submission - Kids With the Greatest Number of Candies (python3)

---
+++ problems/kids_with_the_greatest_number_of_candies/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        
+        mx = max(candies)
+        res = []
+        
+        for candy in candies:
+            
+            if candy+extraCandies >= mx:
+                res.append(True)
+            else:
+                res.append(False)
+            
+        return res
