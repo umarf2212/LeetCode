816db0cd04f4a2aabfd0a7851d573085fd102dbd
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2021-08-08 04:54:24 +0000

    [LeetCode Sync] Runtime - 84 ms (13.39%), Memory - 14.2 MB (100.00%)

--- my-folder/1574-maximum-product-of-two-elements-in-an-array/solution.py
+++ my-folder/1574-maximum-product-of-two-elements-in-an-array/solution.py
@@ -1,12 +1,8 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         
-        maxNum = float('-inf')
+        heap = [-i for i in nums]
         
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i != j:
-                    maxNum = max(maxNum, (nums[i]-1)*(nums[j]-1))
-                    
-        return maxNum
+        heapq.heapify(heap)
         
+        return (-(heapq.heappop(heap))-1) * (-(heapq.heappop(heap))-1)
