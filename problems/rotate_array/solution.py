5194a902e740513f70e0a818fb3f3b6a8f73d002
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2021-08-24 13:49:10 +0000

    Sync LeetCode submission - Rotate Array (python3)

---
+++ problems/rotate_array/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        n = len(nums)-1
+        
+        k = k % len(nums)
+        
+        temp = []
+        i = 0
+        for i in range(len(nums)-1, n-k,-1):
+            temp.append(nums[i])
+        
+        i-=1
+        j = n
+        while i >= 0:
+            nums[j]  = nums[i]
+            i-=1
+            j-=1
+
+        for i in range(k):
+            nums[i] = temp.pop()
+            
