a02ac88ae0bcc6d3a588f38cd3f0cc955b6d922f
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2022-01-01 11:14:56 +0000

    [LeetCode Sync] Runtime - 73 ms (41.50%), Memory - 15.4 MB (100.00%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,9 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
         d = {}
+        
         for i in range(len(nums)):
-            if target - nums[i] in d:
-                return [d[target - nums[i]], i]
-            else:
+            if target - nums[i] not in d:
                 d[nums[i]] = i
-        return
+            else:
+                return [d[target-nums[i]], i]
