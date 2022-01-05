638ce2010e644a207676e12f833917c4186e88d4
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2022-01-05 10:56:09 +0000

    Sync LeetCode submission - Same Tree (python3)

--- problems/same_tree/solution.py
+++ problems/same_tree/solution.py
@@ -5,36 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def traverse(self, t1, t2):
-        if not t1 and not t2:
-            return True
-        elif not t1 or not t2:
-            return False
-        elif t1.val != t2.val:
-            return False
-        else:
-            return self.traverse(t1.left, t2.left) and self.traverse(t1.right, t2.right)
-    
-    def preorder(self, root, res):
-        if not root: 
-            res.append(root)
-            return
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         
-        res.append(root.val)
-        self.preorder(root.left, res)
-        self.preorder(root.right, res)
-
+        def traverse(root1, root2):
+            if not root1 and not root2: return True
+            if not root1 or not root2: return False
+            
+            if root1.left and not root2.left or \
+            not root1.left and root2.left or \
+            root1.right and not root2.right or \
+            not root1.right and root2.right:
+                return False
+            
+            if (root1.left and root2.left and root1.left.val != root2.left.val) or \
+            (root1.right and root2.right and root1.right.val != root2.right.val):
+                return False
+            
+            return traverse(root1.left, root2.left) and \
+        traverse(root1.right, root2.right)
         
-    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        if not p and not q: return True
-        if not p or not q: return False
-        
-        ar1 = []
-        ar2 = []
-        
-        self.preorder(p, ar1)
-        self.preorder(q, ar2)
-        
-        return ar1==ar2
-        
-        # return self.traverse(p, q)
+        if p and q and p.val != q.val: return False
+        return traverse(p, q)
