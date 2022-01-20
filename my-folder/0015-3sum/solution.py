264447f786e2056c1589735da36a3ebcc0008d69
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2022-01-20 09:45:44 +0000

    [LeetCode Sync] Runtime - 916 ms (33.83%), Memory - 17.5 MB (100.00%)

--- my-folder/0015-3sum/solution.py
+++ my-folder/0015-3sum/solution.py
@@ -7,23 +7,31 @@ class Solution:
         
         for i in range(len(nums)):
             
+            if i > 0 and nums[i] == nums[i-1]: 
+                continue
+            
             j = i+1
             k = len(nums)-1
-            
+        
             while j < k:
-                
                 Sum = nums[i] + nums[j] + nums[k]
-                
+
                 if Sum == 0:
                     result.add((nums[i], nums[j], nums[k]))
-                    j+=1
-                    k-=1
-                
+                    
+                    while j < k and nums[j] == nums[j+1]:
+                        j+=1
+                    
+                    while j < k and nums[k] == nums[k-1]:
+                        k-=1
+                    
+                    j += 1
+                    k -= 1
+
                 elif Sum < 0:
                     j += 1
-                    
+
                 else:
                     k -= 1
-                    
         
         return result
