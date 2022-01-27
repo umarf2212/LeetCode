6598c2a1535af94df68df59b32a90e4b8a556e5d
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2022-01-27 18:26:00 +0000

    [LeetCode Sync] Runtime - 805 ms (5.05%), Memory - 23 MB (8.33%)

--- my-folder/1427-all-elements-in-two-binary-search-trees/solution.py
+++ my-folder/1427-all-elements-in-two-binary-search-trees/solution.py
@@ -12,14 +12,29 @@ class Solution:
         self.traverse(root.left, ar)
         ar.append(root.val)
         self.traverse(root.right, ar)
+
+    def merge(self, q1, q2):
+        newAr = []
+        
+        while q1 and q2:
+            if q1[0] < q2[0]:
+                newAr.append(q1.popleft())
+            else:
+                newAr.append(q2.popleft())
+        
+        if q1:
+            newAr += list(q1)
+        elif q2:
+            newAr += list(q2)
+        
+        return newAr
     
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
         
-        res = []
+        ar1 = deque()
+        self.traverse(root1, ar1)
         
-        self.traverse(root1, res)
-        self.traverse(root2, res)
+        ar2 = deque()
+        self.traverse(root2, ar2)
         
-        res.sort()
-        
-        return res
+        return self.merge(ar1, ar2)
