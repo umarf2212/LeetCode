42831297d1c78e465014a7d6d1f35a8e92ac1eae
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2022-03-13 10:10:08 +0000

    [LeetCode Sync] Runtime - 1092 ms (5.00%), Memory - 39 MB (5.25%)

--- my-folder/0234-palindrome-linked-list/solution.py
+++ my-folder/0234-palindrome-linked-list/solution.py
@@ -4,20 +4,37 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def isPalindrome(self, head: ListNode) -> bool:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
         
-        stack = []
         
-        temp = head
+        # find middle node
+        slow = head
+        fast = head
+        while slow and fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+
+        # Reverse the list from middle (slow)
+        prev = slow
+        temp = slow.next
+        slow.next = None
         while temp:
-            stack.append(temp.val)
-            temp = temp.next
+            tempNext = temp.next
+            temp.next = prev
+            prev = temp
+            temp = tempNext
         
-        temp = head
-        while temp:
-            if temp.val != stack.pop():
+        # prev is the tail
+        
+        left = head
+        right = prev
+        
+        while right:
+            if left.val != right.val:
                 return False
             
-            temp = temp.next
-            
+            left = left.next
+            right = right.next
+        
         return True
