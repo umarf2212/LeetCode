9df1a3a3e7f84f0bb17cc041671d13fb8a4a7e12
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2022-04-04 14:39:01 +0000

    [LeetCode Sync] Runtime - 1688 ms (5.06%), Memory - 48.6 MB (7.30%)

--- my-folder/0528-swapping-nodes-in-a-linked-list/solution.py
+++ my-folder/0528-swapping-nodes-in-a-linked-list/solution.py
@@ -4,33 +4,14 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def swapNodes(self, head: ListNode, k: int) -> ListNode:
-        
-        k_first = 0
-        k_last = 0
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         
         temp = head
-        totalNodes = 0
+        ar = []
         while temp:
-            totalNodes += 1
-            
-            if totalNodes == k:
-                k_first = temp
-            
+            ar.append(temp)
             temp = temp.next
-            
         
-        temp = head
-        rev_count = 0
-        while temp:
-            rev_count += 1
-            
-            if rev_count == totalNodes - k + 1:
-                prev_val = temp.val
-                temp.val = k_first.val
-                k_first.val = prev_val
-                
-                
-            temp = temp.next
+        ar[k-1].val, ar[len(ar)-k].val = ar[len(ar)-k].val, ar[k-1].val
         
         return head
