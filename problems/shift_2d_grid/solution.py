8a9fd082f1167d20e93739467c2549697b6c8fc1
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2022-04-11 19:03:40 +0000

    Sync LeetCode submission - Shift 2D Grid (python3)

--- problems/shift_2d_grid/solution.py
+++ problems/shift_2d_grid/solution.py
@@ -3,27 +3,13 @@ class Solution:
         
         flat = [j for i in grid for j in i]
         
-        matrix = []
+        k = len(flat) - k % len(flat)
         
-        row = []
-        count = 0
-        i = len(flat) - k % len(flat)
-        start = i
-        while 1:
-            if i == len(flat):
-                i = 0
-                
-            row.append(flat[i])
-            count += 1
-            
-            if count == len(grid[0]):
-                matrix.append(row)
-                row = []
-                count = 0
-            
-            if i == start-1:
-                break
-                
-            i += 1
+        flat = flat[k:] + flat[:k]
         
-        return matrix
+        res = []
+        cols = len(grid[0])
+        for i in range(0, len(flat), cols):
+            res.append(flat[i:i+cols])
+        
+        return res
