629cb374e2cbecf0c871840b8aa54067be0fbc97
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2022-05-11 07:12:17 +0000

    [LeetCode Sync] Runtime - 97 ms (19.40%), Memory - 15.6 MB (100.00%)

--- my-folder/1221-element-appearing-more-than-25-in-sorted-array/solution.py
+++ my-folder/1221-element-appearing-more-than-25-in-sorted-array/solution.py
@@ -1,12 +1,9 @@
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
         
-        d = {}
-        for num in arr:
-            if num not in d:
-                d[num] = 0
-            d[num] += 1
+        d = Counter(arr)
+        ratio = len(arr) / 4
     
         for num, count in d.items():
-            if count * 100 / len(arr) > 25:
+            if count > ratio:
                 return num
