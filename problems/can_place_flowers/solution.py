6314eebf5d24fdcf96004623501ccccb040b1b23
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2022-05-23 08:01:55 +0000

    Sync LeetCode submission - Can Place Flowers (python3)

--- problems/can_place_flowers/solution.py
+++ problems/can_place_flowers/solution.py
@@ -1,26 +1,24 @@
 class Solution:
-    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:       
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         
-        if len(flowerbed) == 1: return True if (flowerbed[0] == 0 or (flowerbed[0]==1 and n==0)) else False
-        
-        i = 0
-        
-        if flowerbed[0] == 0 and flowerbed[1]==0:
-            flowerbed[0] = 1
-            n -= 1
-
-        while i <= len(flowerbed)-2:
+        if len(flowerbed)==1 and flowerbed[0] == 0:
+            n-=1 
             
-            if flowerbed[i-1] == 0 and flowerbed[i+1] == 0 and flowerbed[i] == 0:
-                flowerbed[i] = 1
-                n -= 1
-            i += 1
         
-        if flowerbed[i] == 0 and flowerbed[i-1]==0:
-            flowerbed[0] = 1
-            n -= 1
+        elif flowerbed[0]==0 and flowerbed[1]==0:
+            flowerbed[0]=1
+            n-=1
         
-        if n>0:
-            return False
+        i=1
+        while i < len(flowerbed)-1:
+            if flowerbed[i-1]==0 and flowerbed[i+1]==0 and flowerbed[i]==0:
+                flowerbed[i]=1
+                n-=1
+            
+            i+=1
         
-        return True
+        if len(flowerbed)>2 and flowerbed[-1]==0 and flowerbed[-2]==0:
+            n-=1
+        
+        
+        return n <= 0
