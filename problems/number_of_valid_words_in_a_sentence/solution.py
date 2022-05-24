570849fb51be866d9468f53e42f7f5c3a6c3d3bd
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2022-05-24 02:40:53 +0000

    Sync LeetCode submission - Number of Valid Words in a Sentence (python3)

--- problems/number_of_valid_words_in_a_sentence/solution.py
+++ problems/number_of_valid_words_in_a_sentence/solution.py
@@ -1,38 +1,30 @@
 class Solution:
     def countValidWords(self, sentence: str) -> int:
         
+        ar = sentence.split()
+        
         count = 0
-        punc = '.,!'
-        digits = set(['0','1','2','3','4','5','6','7','8','9'])
-        
-        if len(sentence) == 1 and sentence[0] in punc:
-            return 1
-        
-        for word in sentence.split(' '):
-            if len(word) == 0: continue
-                
-            hyphen = 0
-            if word[0] == '-' or word[-1] == '-':
+        for word in ar:
+            
+            if word[0] == '-' or word[-1] == '-' or word[-1].isdigit():
                 continue
             
-            n = len(word)
-            noword = False
-            for i in range(n):
+            isWord = True
+            hyphen = 0
+            for i in range(len(word)-2,-1,-1):
                 
-                #if any digits found, leave
-                if word[i] in digits:
-                    noword = True
-                
-                #check for hyphens, also if more than one
                 if word[i] == '-':
-                    hyphen += 1
-                    if hyphen > 1 or word[i-1] in punc or word[i+1] in punc: 
-                        noword = True
-                        
-                #check for punctuation in the middle
-                if i < n-1 and word[i] in punc:
-                    noword = True
+                    if i>0 and not word[i-1].isalpha() or not word[i+1].isalpha():
+                        isWord=False
+                    else:
+                        hyphen += 1
                     
-            if not noword: count += 1
-            
+                elif word[i].isdigit() or word[i] in '!.,':
+                    isWord = False
+                
+            if not isWord or hyphen > 1:
+                continue
+
+            count += 1
+        
         return count
