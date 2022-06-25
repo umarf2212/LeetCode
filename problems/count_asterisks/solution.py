7ef7519d477dc2469bf843e3eafe78454fdfc22c
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2022-06-25 14:35:08 +0000

    Sync LeetCode submission - Count Asterisks (python3)

---
+++ problems/count_asterisks/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countAsterisks(self, s: str) -> int:
+        
+        open = False
+        res = 0
+        for ch in s:
+            
+            if ch == '|':
+                open = not open
+            
+            if ch == '*' and not open:
+                res += 1
+        
+        return res
