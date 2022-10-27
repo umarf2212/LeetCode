0a13f8ed1ddfb81226b46d3947910c434b3b2ee0
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2022-10-27 20:17:16 +0000

    [LeetCode Sync] Runtime - 193 ms (6.40%), Memory - 14.1 MB (100.00%)

--- my-folder/0733-flood-fill/solution.py
+++ my-folder/0733-flood-fill/solution.py
@@ -1,20 +1,21 @@
 class Solution:
-    
-    def fill(self, image, r, c, startPixel, newColor):
-        if (r < 0 or r >= len(image)) or (c < 0 or c >= len(image[0])): return False
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+
+        def fill(image, sr, sc, color, startColor):
+            if sr < 0 or sr >= len(image) or sc < 0 or sc >= len(image[0]):
+                return
+            
+            if image[sr][sc] != startColor or image[sr][sc] == color:
+                return
         
-        if image[r][c] == startPixel:
-            image[r][c] = newColor
+            image[sr][sc] = color
+
+            fill(image, sr-1, sc, color, startColor)
+            fill(image, sr+1, sc, color, startColor)
+            fill(image, sr, sc-1, color, startColor)
+            fill(image, sr, sc+1, color, startColor)
         
-            self.fill(image, r-1, c, startPixel, newColor)
-            self.fill(image, r+1, c, startPixel, newColor)
-            self.fill(image, r, c-1, startPixel, newColor)
-            self.fill(image, r, c+1, startPixel, newColor)
-        
-    
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        
-        if image[sr][sc] == newColor: return image
-        
-        self.fill(image, sr, sc, image[sr][sc], newColor)
+        startColor = image[sr][sc]
+        fill(image, sr, sc, color, startColor)
         return image
+
