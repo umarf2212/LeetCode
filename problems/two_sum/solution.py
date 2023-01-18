3f3605c678acefa566ef24117bff4ac62e9079c9
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2023-01-18 09:14:50 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -2,9 +2,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
         d = {}
-        
         for i in range(len(nums)):
-            if target - nums[i] not in d:
-                d[nums[i]] = i
+            if target - nums[i] in d:
+                return [i, d[target - nums[i]]]
             else:
-                return [d[target-nums[i]], i]
+                d[nums[i]] = i
