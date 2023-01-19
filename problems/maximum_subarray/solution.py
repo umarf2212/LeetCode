abf9db06a528c0ec56a3fc55dc14307e5242beda
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2023-01-19 14:39:15 +0000

    Sync LeetCode submission - Maximum Subarray (python3)

--- problems/maximum_subarray/solution.py
+++ problems/maximum_subarray/solution.py
@@ -1,28 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        
-        maxNegative = float('-inf')
-        for i in range(len(nums)):
-            if nums[i] > maxNegative:
-                maxNegative = nums[i]
-            
-            if nums[i] > 0:
-                break
-        
-        if i == len(nums)-1 and nums[i] < 0:
-            return maxNegative
-        
-        sumSoFar = 0
-        maxSum = 0
-        
-        for i in range(len(nums)):
-            
-            if sumSoFar + nums[i] > 0:
-                sumSoFar += nums[i]
-            else:
-                sumSoFar = 0
-            
-            if sumSoFar > maxSum:
-                maxSum = sumSoFar
+
+        sumSoFar = nums[0]
+        maxSum = nums[0]
+        for i in range(1, len(nums)):
+            sumSoFar = max(nums[i], sumSoFar + nums[i])
+            maxSum = max(maxSum, sumSoFar)
         
         return maxSum
