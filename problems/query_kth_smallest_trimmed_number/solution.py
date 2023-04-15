c7ce14443fb19ae657835c73c60f57f4543c8135
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2023-04-15 10:24:44 +0000

    Sync LeetCode submission - Query Kth Smallest Trimmed Number (python3)

---
+++ problems/query_kth_smallest_trimmed_number/solution.py
@@ -0,0 +1,20 @@
+import heapq
+class Solution:
+    def smallestTrimmedNumbers(self, nums: List[str], queries: List[List[int]]) -> List[int]:
+        
+        ans = []
+        for query in queries:
+            k = query[0]
+            trim = query[1]
+
+            trimmedNums = [(int(nums[i][len(nums[i])-trim:]), i) for i in range(len(nums))]
+            
+            heapq.heapify(trimmedNums)
+            
+            while k > 1:
+                heapq.heappop(trimmedNums)
+                k -= 1
+            
+            ans.append(trimmedNums[0][1])
+
+        return ans
