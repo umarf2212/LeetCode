94a697381534efc22a2c96f6d41ab6e94d74486d
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2023-04-24 05:08:22 +0000

    Sync LeetCode submission - Partition Labels (python3)

--- problems/partition_labels/solution.py
+++ problems/partition_labels/solution.py
@@ -1,37 +1,33 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        
+
         intervals = {}
-        seen = set()
         for i in range(len(s)):
             if s[i] not in intervals:
-                j = len(s)-1
-                while s[j] != s[i]:
-                    j-=1
-                
-                intervals[s[i]] = [i,j]
+                intervals[s[i]] = [i,i]
+            else:
+                intervals[s[i]][1] = i
         
+
         intervals = list(intervals.values())
-        intervals.sort(key=lambda x: x[0])
+        intervals.sort(key = lambda x:x[0])
+        mergedIntervals = [intervals[0]]
+        for i in range(1, len(intervals)):
+            cur = intervals[i]
+            prev = mergedIntervals[-1]
+
+            # prev->last | cur->first
+            if prev[1] > cur[0]:
+                mergedIntervals.pop()
+                new = [ prev[0], max(prev[1], cur[1]) ]
+                mergedIntervals.append(new)
+            else:
+                mergedIntervals.append(cur)
+
+        res = []
+        for interval in mergedIntervals:
+            res.append(interval[1]-interval[0]+1)
         
-        # print(intervals)
-        
-        mergedIntervals = []
-        i=0
-        while i < len(intervals)-1:
-            start = intervals[i][0]
-            currEnd = intervals[i][1]
-            # if intervals overlap
-            if intervals[i][1] >= intervals[i+1][0]:
-                currEnd = intervals[i][1]
-                while i<len(intervals)-1 and currEnd >= intervals[i+1][0]:
-                    currEnd = max(currEnd, intervals[i+1][1])
-                    i+=1
-            
-            mergedIntervals.append(currEnd-start+1)
-            i += 1
-        
-        if i < len(intervals):
-            mergedIntervals.append(intervals[i][1]-intervals[i][0]+1)
-            
-        return mergedIntervals
+        return res
+
+
