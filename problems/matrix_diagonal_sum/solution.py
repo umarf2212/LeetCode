8ce23c9bfb146698d43edf42c1170440566ceb92
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2023-05-08 20:14:05 +0000

    Sync LeetCode submission - Matrix Diagonal Sum (python3)

--- problems/matrix_diagonal_sum/solution.py
+++ problems/matrix_diagonal_sum/solution.py
@@ -1,14 +1,40 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
-        summ = 0
-        c = 0
-        for r in range(len(mat)):
-            summ += mat[r][c]
-            c+=1
+
+        i = 0
+        j = 0
+        Sum = 0
+        while i < len(mat) and j < len(mat[0]):
+            Sum += mat[i][j]
+            i+=1
+            j+=1
         
-        c = len(mat)-1
-        for r in range(len(mat)):
-            summ += mat[r][c]
-            c-=1
+        i = 0
+        j = len(mat[0])-1
+        while i < len(mat) and j >= 0:
+            Sum += mat[i][j]
+            i+=1
+            j-=1
         
-        return summ - mat[len(mat)//2][len(mat[0])//2] if len(mat)%2!=0 else summ
+        if len(mat) % 2 != 0:
+            c = len(mat)//2
+            Sum -= mat[c][c]
+        
+        return Sum
+
+        
+
+        # 0 0 0 0
+        # 0 0 0 0
+        # 0 0 0 0
+        # 0 0 0 0
+
+        # 0 0 0 0 0 0 0 0
+        # 0 0 0 0 0 0 0 0
+        # 0 0 0 0 0 0 0 0
+        # 0 0 0 0 0 0 0 0
+        # 0 0 0 0 0 0 0 0
+        # 0 0 0 0 0 0 0 0
+        # 0 0 0 0 0 0 0 0
+        # 0 0 0 0 0 0 0 0
+        
