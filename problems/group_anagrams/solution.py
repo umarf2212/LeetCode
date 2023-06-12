fdca28c93d09bea35a5a3186e08db45bb46af024
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2023-06-12 12:11:27 +0000

    Sync LeetCode submission - Group Anagrams (python3)

---
+++ problems/group_anagrams/solution.py
@@ -0,0 +1,15 @@
+from collections import Counter
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        d = {}
+        for i in range(len(strs)):
+            key = ''.join(sorted(strs[i]))
+
+            if key not in d:
+                d[key] = []
+            d[key].append(strs[i])
+        
+        return d.values()
+
+
