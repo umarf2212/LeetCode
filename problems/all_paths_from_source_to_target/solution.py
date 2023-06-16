a28be8386321e96051083239180b46570410fe63
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2023-06-16 16:56:06 +0000

    Sync LeetCode submission - All Paths From Source to Target (python3)

---
+++ problems/all_paths_from_source_to_target/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+
+        def traverse(graph, i, path, ans):
+
+            # found the path from 0 to n-1
+            if i >= len(graph)-1:
+                ans.append(path[:])
+                return
+            
+            for j in range(len(graph[i])):
+                path.append(graph[i][j])
+                traverse(graph, graph[i][j], path, ans)
+                path.pop()
+        
+        ans = []
+        path = [0]
+        traverse(graph, 0, path, ans)
+        return ans
+
