b3aa5d62ec9ffef97583f5f490167441a3fb79ff
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2023-08-05 15:07:25 +0000

    [LeetCode Sync] Runtime - 110 ms (13.34%), Memory - 21.2 MB (5.71%)

---
+++ my-folder/2903-insert-greatest-common-divisors-in-linked-list/solution.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head.next:
+            return head
+        
+        def GCD(a, b):
+            if b == 0:
+                return a
+            return GCD(b, a%b)
+        
+        # 18 6 10 3
+        
+        # 18 6 10
+        
+        temp = head
+        while temp.next:
+            tempNext = temp.next
+            
+            gcd = GCD(temp.val, tempNext.val)
+            
+            newNode = ListNode(gcd)
+            
+            temp.next = newNode
+            newNode.next = tempNext
+            temp = tempNext
+        
+        return head
+            
+            
