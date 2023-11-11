88ffcf5ea7efccef8af0ae7fc45488bed566f1d8
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2023-11-11 11:05:39 +0000

    Sync LeetCode submission - Longest Palindromic Subsequence (python3)

--- problems/longest_palindromic_subsequence/solution.py
+++ problems/longest_palindromic_subsequence/solution.py
@@ -1,17 +1,29 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
+
+        # Recurrence Relation:
+        # dp[i][j] :-- 
+        # if A[i] == A[j] : 2 + dp[i+1][j-1]
+        # else max( dp[i+1][j], dp[i][j-1] )
+
+        # (i, j) depends on => i+1 and j-1
         
-        m = len(s)
+        n = len(s)
+
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+
+        # LPS of length 1 = 1
+        for i in range(n):
+            dp[i][i] = 1
         
-        dp = [[0 for _ in range(m+1)] for _ in range(m+1)]
-                
-        for i in range(1, m+1):
-            for j in range(1, m+1):
-                
-                if s[i-1] == s[m-j]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
+        # for checking LPS of lens from 2 to n itself
+        for length in range(2, n+1):
+            for i in range(n - length + 1): # +1 cos it's upper limit
+                j = i + length - 1 # -1 cos it's index
+
+                if s[i] == s[j]:
+                    dp[i][j] = 2 + dp[i+1][j-1]
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-                    
+                    dp[i][j] = max( dp[i+1][j], dp[i][j-1] )
         
-        return dp[m][m]
+        return dp[0][n-1]
