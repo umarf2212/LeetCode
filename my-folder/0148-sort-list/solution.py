91e534fa04b49fa79e1c7658c53f4a3bbad7e1b1
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2023-12-30 10:01:19 +0000

    [LeetCode Sync] Runtime - 483 ms (6.89%), Memory - 40.5 MB (5.05%)

--- my-folder/0148-sort-list/solution.py
+++ my-folder/0148-sort-list/solution.py
@@ -4,21 +4,58 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def sortList(self, head: ListNode) -> ListNode:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        temp = head
-        ar = []
-        while temp:
-            ar.append(temp.val)
-            temp = temp.next
+        # Merge Sort - divide and conquer
+
+        # 1. Divide 
+        # 2. Merge
+
+        # 4 -> 2 -> 1 -> 3
+        # 4 -> 2 -> 1 -> 3 -> 5
+
+        # fast = head
+        # slow = head
+        # while fast and fast.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
         
-        ar.sort()
+        # slow is mid node
+        def divide(head):
+            fast = head
+            slow = head
+            while fast and fast.next:
+                prev = slow
+                slow = slow.next
+                fast = fast.next.next
+            
+            prev.next = None
+            
+            return slow
         
-        temp = head
-        c = 0
-        while temp:
-            temp.val = ar[c]
-            c += 1
-            temp = temp.next
+        def MergeSort(head):
+            if not head or not head.next:
+                return head
+            
+            mid = divide(head)
+
+            left = MergeSort(head)
+            right = MergeSort(mid)
+
+            return merge(left, right)
         
-        return head
+        # 1 -> 4 -> 8
+        # 2 -> 3 -> 5
+        
+        def merge(A, B):
+            if not A: return B
+            if not B: return A
+
+            small = A if A.val <= B.val else B
+            big = A if A.val > B.val else B
+
+            small.next = merge(small.next, big)
+            return small
+        
+        return MergeSort(head)
+
