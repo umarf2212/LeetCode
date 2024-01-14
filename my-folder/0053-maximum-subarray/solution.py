3e12b12e77a6cd40efa5d6c8d118492fd760a07f
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-01-14 06:46:12 +0000

    [LeetCode Sync] Runtime - 572 ms (11.47%), Memory - 31.8 MB (16.55%)

--- my-folder/0053-maximum-subarray/solution.py
+++ my-folder/0053-maximum-subarray/solution.py
@@ -1,10 +1,16 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-
-        sumSoFar = nums[0]
-        maxSum = nums[0]
-        for i in range(1, len(nums)):
-            sumSoFar = max(nums[i], sumSoFar + nums[i])
-            maxSum = max(maxSum, sumSoFar)
         
-        return maxSum
+        curSum = nums[0]
+        sumSoFar = nums[0]
+
+        for i in range(1, len(nums)):
+
+            # does current element increase the overall curSum?
+            # or is it greater than the curSum in itself?
+            curSum = max(nums[i], curSum + nums[i])
+
+            # is the curSum the max we've seen so far?
+            sumSoFar = max(sumSoFar, curSum)
+        
+        return sumSoFar
