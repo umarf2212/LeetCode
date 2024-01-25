c1cf618fb956521241ca08c18ca3205af18b8729
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-01-25 09:08:58 +0000

    [LeetCode Sync] Runtime - 783 ms (24.46%), Memory - 27.6 MB (11.31%)

--- my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,18 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        if len(prices) in [0,1]: return 0        
-        
-        max_diff = 0
-        min_elem = prices[0]
-        
-        for i in range(1, len(prices)):
-            
-            if prices[i] < min_elem:
-                min_elem = prices[i]
-                
-            if prices[i] - min_elem > max_diff:
-                max_diff = prices[i] - min_elem
-                
-        
-        return max_diff
+        curMin = prices[0]
+        maxDiff = 0
+        for price in prices:
+            curMin = min(curMin, price)
+            maxDiff = max(maxDiff, price-curMin)
+
+        return maxDiff
