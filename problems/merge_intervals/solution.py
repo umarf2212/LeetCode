daf8eff8ecd01895a69d9913db6ac7516caf43a1
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-01-27 12:31:05 +0000

    Sync LeetCode submission - Merge Intervals (python3)

--- problems/merge_intervals/solution.py
+++ problems/merge_intervals/solution.py
@@ -2,19 +2,23 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
 
         intervals.sort(key=lambda x:x[0])
-
+        
         stack = [intervals[0]]
         for i in range(1, len(intervals)):
-            #overlap
-            if intervals[i][0] <= stack[-1][1]:
-                new = [stack[-1][0], max(stack[-1][1], intervals[i][1])]
-                stack.pop()
-                stack.append(new)
+            curStart, curEnd = intervals[i]
+
+            # overlap
+            if stack[-1][1] >= curStart:
+                prevStart, prevEnd = stack.pop()
+
+                newStart = min(prevStart, curStart)
+                newEnd = max(prevEnd, curEnd)
+
+                stack.append( [newStart, newEnd] )
+            
             else:
                 stack.append(intervals[i])
-
+        
         return stack
 
-
-
-
+                
