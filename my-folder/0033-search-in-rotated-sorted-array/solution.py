7b70f26b6ba98ea45c481e2937c51ab190657ff3
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-01-27 08:05:26 +0000

    [LeetCode Sync] Runtime - 40 ms (76.20%), Memory - 16.9 MB (54.57%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.py
+++ my-folder/0033-search-in-rotated-sorted-array/solution.py
@@ -1,49 +1,46 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
-        # 0 1 2 3 4 5 6 7 8
-        # 3 4 5 6 7 8 0 1 2
 
-        def pivotPoint(nums):
+        if len(nums) == 1:
+            return 0 if nums[0] == target else -1
+        
+        def findPivot(nums):
             i = 0
             j = len(nums)-1
+
             while i <= j:
                 mid = (i+j) // 2
 
-                # naive idea is to check if pivot is greater than both it's 
-                # adjacent elements,
-                # and intuitive idea is that since array is sorted, we only need 
-                # to check if nums[mid] > nums[mid+1] to consider it pivot
                 if mid == len(nums)-1 or nums[mid] > nums[mid+1]:
                     return mid
                 
-                if nums[mid] >= nums[i]:
-                    i = mid + 1
+                if nums[i] <= nums[mid]:
+                    i = mid+1
                 else:
-                    j = mid - 1
+                    j = mid-1
         
-        def binarySearch(nums, i, j, target):
-            while i <= j:
-                mid = i + (j-i) // 2
+        def binarySearch(nums, target, lo, hi):
+            while lo <= hi:
+                mid = (lo+hi) // 2
 
                 if nums[mid] == target:
                     return mid
                 
-                if nums[mid] > target:
-                    j = mid - 1
+                if nums[mid] < target:
+                    lo = mid+1
                 else:
-                    i = mid + 1
+                    hi = mid-1
+            
             return -1
+        
+        pivot = findPivot(nums)
+        print(pivot)
 
-        pivot = pivotPoint(nums)
-        if nums[pivot] == target:
-            return pivot
-
-        left = binarySearch(nums, 0, pivot, target)
-        right = binarySearch(nums, pivot+1, len(nums)-1, target)
+        left = binarySearch(nums, target, 0, pivot)
+        right = binarySearch(nums, target, pivot+1, len(nums)-1)
 
         if left != -1:
             return left
+        
         return right
-                
 
