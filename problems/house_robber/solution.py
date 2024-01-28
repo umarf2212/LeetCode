f804ab7fd9403cc907f85e66ea228c9dff576abf
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-01-28 08:25:11 +0000

    Sync LeetCode submission - House Robber (python3)

--- problems/house_robber/solution.py
+++ problems/house_robber/solution.py
@@ -1,22 +1,32 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        '''
-            1 2 3 6 4    8 7 3 2
-                    i-1  i
+        # def ml(i):
+        #     if i < 0:
+        #         return 0
             
-            M(i) = max( nums[i] + M(i-2) , M(i-1) )
-            M(0) = nums[0]
-            M(1) = max(nums[0], nums[1])
+        #     if dp[i] != -1:
+        #         return dp[i]
             
-        '''
-        if len(nums) == 1: return nums[0]
+        #     dp[i] = max(ml(i-1), nums[i] + ml(i-2))
+
+        #     return dp[i]
         
-        dp = [0 for _ in range(len(nums))]
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
+        n = len(nums)
+        dp = [-1] * n
+
+        # return ml(n-1)
+
+        A = nums
+
+        if len(A) == 1: return A[0]
+        if len(A) == 2: return max(A[0], A[1])
+
+        dp[0] = A[0]
+        dp[1] = max(A[0], A[1])
+
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], A[i]+dp[i-2])
         
-        for i in range(2, len(nums)):
-            dp[i] = max( dp[i-1] , nums[i] + dp[i-2] )
-            
-        return dp[len(nums)-1]
+        return dp[n-1]
+
