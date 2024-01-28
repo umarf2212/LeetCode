3071c9663df4385e49a451a8ca593e56362ce164
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-01-28 07:03:55 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- problems/climbing_stairs/solution.py
+++ problems/climbing_stairs/solution.py
@@ -1,10 +1,22 @@
 class Solution:
-    
-    @cache
-    def steps(self, n):
-        if n <= 3: return n 
-        return self.steps(n-1) + self.steps(n-2)
-    
     def climbStairs(self, n: int) -> int:
+        
+        def waysToClimb(x):
+            # Base Case
+            if x <= 3: return x
             
-        return self.steps(n)
+            # Check if memoised
+            if dp[x] != -1:
+                return dp[x]
+            
+            # Calculate
+            res = waysToClimb(x-1) + waysToClimb(x-2)
+
+            # Store/memoise
+            dp[x] = res
+
+            # Return
+            return res
+        
+        dp = [-1] * (n+1)
+        return waysToClimb(n)
