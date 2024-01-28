1e3c68681144f922f67de253138550c048f3d8bc
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-01-28 08:40:04 +0000

    Sync LeetCode submission - Unique Paths (python3)

--- problems/unique_paths/solution.py
+++ problems/unique_paths/solution.py
@@ -1,26 +1,31 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
-        m, n = max(m, n), min(m, n)
-        
-        dp = [[0 for _ in range(n)] for _ in range(m)]
-        dp = [0 for _ in range(n)]
+        # def paths(i, j):
+        #     if i == 0 or j == 0:
+        #         return 1
+            
+        #     if dp[i][j] != -1:
+        #         return dp[i][j]
+            
+        #     dp[i][j] = paths(i-1, j) + paths(i, j-1)
 
-        # dp[0][0] = 1
-        dp[0] = 1
-
-        # for i in range(1, m):
-        #     dp[i][0] = 1
+        #     return dp[i][j]
         
-        for j in range(1, n):
-            dp[j] = 1
+
+        # dp = [[-1 for _ in range(n)] for _ in range(m)]
+        # return paths(m-1, n-1)
+
+        dp = [[-1 for _ in range(n)] for _ in range(m)]
+        
+        for i in range(m):
+            dp[i][0] = 1
+        
+        for j in range(n):
+            dp[0][j] = 1
         
         for i in range(1, m):
-            curRow = [1]
             for j in range(1, n):
-                curRow.append(curRow[-1] + dp[j])
-            
-            dp = [i for i in curRow]
-
-        return dp[-1]
-
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        return dp[m-1][n-1]
