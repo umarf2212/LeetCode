7ef811e8df8d1f887f7255f729a6bd6cd8b57764
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-01-28 09:25:51 +0000

    [LeetCode Sync] Runtime - 1199 ms (13.96%), Memory - 18.9 MB (29.41%)

--- my-folder/0322-coin-change/solution.py
+++ my-folder/0322-coin-change/solution.py
@@ -1,26 +1,35 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
-        '''
-        Recurrence Relation:
-        M(i,j) = min( M(i-1, j) , 1 + M(i, j-coins[i]) )
+        # Recurrence Relation
+        # M(i, amount) = min( M(i-1, curAmount), 1 + M(i, curAmount-coin[i]) )
         
-        '''
-        
+        # Classic 0/1 problem of choose or not choose
+
         n = len(coins)
+
+        dp = [[0 for _ in range(amount+1)] for _ in range(n+1)]
+
+        #no coins needed for 0 amount
+        dp[0][0] = 0 
+
+        # with no coins, no amount can be made
+        # inf is the *right* placeholder as per the "RR"
+        for i in range(1, amount+1):
+            dp[0][i] = float('inf')
         
-        dp = [0] + [float('inf') for _ in range(amount)]
-        
-        # 0 amount -> coins = 0
-        # 0 amount -> coins > 0 -> float('inf')
-        
-        # dp[0][i] -> float('inf')
-        # dp[i][0] -> 0 amount        
-    
         for i in range(1, n+1):
             for j in range(1, amount+1):
-                if coins[i-1] <= j:
-                    dp[j] = min(dp[j], 1 + dp[j - coins[i-1]])
-          
+                if j >= coins[i-1]:
+                    dp[i][j] = min( dp[i-1][j], 1 + dp[i][j-coins[i-1]] )
+                else:
+                    dp[i][j] = dp[i-1][j]
         
-        return dp[amount] if dp[amount] != float('inf') else -1
+        if dp[n][amount] == float('inf'):
+            return -1
+        
+        return dp[n][amount]
+
+
+
+
