0da4a0490445386a266661e3f623ea7adb9fcd95
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-01-28 08:59:18 +0000

    [LeetCode Sync] Runtime - 623 ms (34.99%), Memory - 43.2 MB (25.32%)

--- my-folder/1250-longest-common-subsequence/solution.py
+++ my-folder/1250-longest-common-subsequence/solution.py
@@ -3,17 +3,26 @@ class Solution:
         
         m = len(text1)
         n = len(text2)
-        
-        dp = [[0 for i in range(n+1)] for j in range(m+1)]
 
-        for i in range(1, m+1):
-            for j in range(1, n+1):
-                
-                if text1[i-1] == text2[j-1]:
-                    dp[i][j] = dp[i-1][j-1] + 1
-                else:
-                    dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+        def lcs(i, j):
+            if i < 0 or j < 0:
+                return 0
+            
+            if dp[i][j] != -1:
+                return dp[i][j]
+            
+            ans = -1
+            if text1[i] == text2[j]:
+                ans = 1 + lcs(i-1, j-1)
+            else:
+                ans = max(lcs(i-1, j), lcs(i, j-1))
+            
+            dp[i][j] = ans
+            return ans
+        
+        dp = [[-1 for _ in range(n)] for _ in range(m)]
 
-        return dp[m][n]
-        
-        
+        return lcs(m-1, n-1)
+
+            
+
