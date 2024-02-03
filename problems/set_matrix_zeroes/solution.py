1a89e17dd1f39034c5f333f1fdd68743f5807c2f
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-02-03 12:44:01 +0000

    Sync LeetCode submission - Set Matrix Zeroes (python3)

---
+++ problems/set_matrix_zeroes/solution.py
@@ -0,0 +1,32 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        inf = float('inf')
+        m, n = len(matrix), len(matrix[0])
+
+        row = [0 for _ in range(m)]
+        col = [0 for _ in range(n)]
+
+        for i in range(m):
+            for j in range(n):    
+                if matrix[i][j] == 0:
+                    row[i] = 1
+                    col[j] = 1
+        
+        for i in range(m):
+            if row[i]:
+                for j in range(n):
+                    matrix[i][j] = 0
+        
+        for j in range(n):
+            if col[j]:
+                for i in range(m):
+                    matrix[i][j] = 0
+                    
+        
+        
+
+                
