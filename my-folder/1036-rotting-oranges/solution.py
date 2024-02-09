bec992c7ce074ba00ec85c03241c33f7a8575975
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-02-09 12:15:29 +0000

    [LeetCode Sync] Runtime - 44 ms (87.74%), Memory - 16.6 MB (64.50%)

--- my-folder/1036-rotting-oranges/solution.py
+++ my-folder/1036-rotting-oranges/solution.py
@@ -1,54 +1,54 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-
-        def isValid(i, j):
-            if i<0 or i>=m or j<0 or j>=n or grid[i][j] == 2 or grid[i][j] == 0 or vis[i][j] == 1:
-                return False
-            return True
+        
         
         m, n = len(grid), len(grid[0])
-
-        dx = [-1, 1, 0, 0]
-        dy = [0, 0, -1, 1]
+        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
         dist = [[float('inf') for _ in range(n)] for _ in range(m)]
-        vis = [[0 for _ in range(n)] for _ in range(m)]
+        vis = [[False for _ in range(n)] for _ in range(m)]
 
         q = deque()
-
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
                     dist[i][j] = 0
-                    q.append( (i, j, 0) )
-                    vis[i][j] = 1
+                    q.append((i, j, 0))
+                    vis[i][j] = True
+            
+        def isValid(i, j):
+            if i<0 or i>=m or j<0 or j>=n or grid[i][j]==0 or grid[i][j]==2 or vis[i][j]:
+                return False
+            return True
         
+
         while q:
             i, j, d = q.popleft()
 
-            dist[i][j] = d
+            # dist[i][j] = d
 
-            for k in range(4):
-                new_I = dx[k] + i
-                new_J = dy[k] + j
+            for dx, dy in dirs:
+                new_i = dx + i
+                new_j = dy + j
 
-                if isValid(new_I, new_J):
-                    new_dist = min( dist[new_I][new_J], d+1 )
-                    q.append( (new_I, new_J, new_dist) )
-                    vis[new_I][new_J] = 1
+                if isValid(new_i, new_j):
+                    new_d = min(d+1, dist[new_i][new_j])
+                    vis[new_i][new_j] = True
+                    dist[new_i][new_j] = new_d
+                    q.append((new_i, new_j, new_d))
         
         ans = -1
         allRotten = True
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    ans = max(dist[i][j], ans)
                     allRotten = False
+
+                if grid[i][j] == 1:
+                    ans = max(ans, dist[i][j])
         
         if allRotten: return 0
-
-        return ans if ans != float('inf') else -1        
-
         
+        return ans if ans != float('inf') else -1
 
