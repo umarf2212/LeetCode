61a0257f79345f3a5e255fc6d128680ecdd4ec4d
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-04-19 20:43:00 +0000

    [LeetCode Sync] Runtime - 182 ms (67.59%), Memory - 17.4 MB (44.97%)

--- my-folder/0547-number-of-provinces/solution.py
+++ my-folder/0547-number-of-provinces/solution.py
@@ -1,26 +1,49 @@
-from collections import defaultdict
+class DSU:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [1 for _ in range(n)]
+    
+    def find(self, v):
+        if self.parent[v] == v:
+            return v
+        
+        self.parent[v] = self.find(self.parent[v])
+        return self.parent[v]
+    
+    def union(self, u, v):
+        x = self.find(u)
+        y = self.find(v)
+
+        if x == y: return
+
+        if self.rank[x] > self.rank[y]:
+            self.parent[y] = x
+        
+        elif self.rank[y] > self.rank[x]:
+            self.parent[x] = y
+        
+        else:
+            self.parent[y] = x
+            self.rank[x] += 1
+
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         
         n = len(isConnected)
-        adj = defaultdict(list)
+
+        dsu = DSU(n)
+
         for i in range(n):
-            for j in range(n):
+            for j in range(i, n):
                 if isConnected[i][j] == 1:
-                    adj[i].append(j)
+                    # i <---edge---> j
+                    dsu.union(i, j)
         
-        def dfs(v):
-            vis.add(v)
-            for u in adj[v]:
-                if u not in vis:
-                    dfs(u)
-        
-        res = 0
-        vis = set()
+        CC = 0
         for i in range(n):
-            if i not in vis:
-                dfs(i)
-                res += 1
-        
-        return res
+            if i == dsu.parent[i]:
+                CC += 1
+            
+        return CC
 
