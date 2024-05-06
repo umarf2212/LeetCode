6a3e426b1e7a6eea0e0da8100c7d2d2e41cf8d27
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-05-06 12:30:41 +0000

    [LeetCode Sync] Runtime - 466 ms (81.14%), Memory - 17.8 MB (90.69%)

---
+++ my-folder/1753-path-with-minimum-effort/solution.py
@@ -0,0 +1,42 @@
+from heapq import heappop, heappush, heapify
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+
+        # DIJKSTRA
+
+        m, n = len(heights), len(heights[0])
+        
+        inf = float('inf')
+
+        # (d, node) -> distance `d` from source to current `node`
+        heap = [(0, 0, 0)]
+        dirs = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        dist = [[inf for _ in range(n)] for _ in range(m)]
+        maxDiff = 0
+
+        while heap:
+            effort, i, j = heappop(heap)
+
+            if i == m-1 and j == n-1:
+                return effort
+
+            if dist[i][j] == inf:
+                dist[i][j] = effort
+
+                for dx, dy in dirs:
+                    new_i = dx + i
+                    new_j = dy + j
+
+                    if 0 <= new_i < m and 0 <= new_j < n:
+                        newEffort = max(effort, abs(heights[new_i][new_j]-heights[i][j]))
+
+                        if dist[new_i][new_j] == inf:
+                            heappush(heap, (newEffort, new_i, new_j) )
+        
+        return maxDiff
+
+
+
+                
+
+
