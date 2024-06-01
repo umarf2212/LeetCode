bdd8ded0850b828f7818ac1b2a5eafddefd83b6e
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-06-01 08:24:46 +0000

    [LeetCode Sync] Runtime - 81 ms (21.39%), Memory - 19.9 MB (18.88%)

--- my-folder/0057-insert-interval/solution.py
+++ my-folder/0057-insert-interval/solution.py
@@ -1,41 +1,59 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        
-        if not intervals: return [newInterval]
-        
-        if newInterval[1] < intervals[0][0]:
-            return [newInterval, *intervals]
-        
-        if newInterval[0] > intervals[-1][1]:
-            intervals.append(newInterval)
-            return intervals
-        
-        res = []
+
+        # [1, 2], [3, 6], [9, 10]
+        # [4, 8]
+
+        # p---q  a-----b  c----d e----------f  g----h
+        #           x---------------------y
+
+
+
+        #       --  ------  -----
+        #    -----------------------
+
+        #     ----------
+        #  --------
+
+        #  -----
+        #           -----
+    
+        # 1. Create result container newIntervals that contains the result
+        # 2. Add all starting intervals that do not overlap with newInterval
+        # 3. Once one interval overlaps with newInterval, we'll merge these two and     
+        #    push them in the newIntervals array.
+        # 4. Now, we simply merge the top of newIntervals with every subsequent 
+        #    interval, if they both overlap
+
+        n = len(intervals)
+        finalIntervals = []
+
+        # Add starting non-overlapping intervals
         i=0
-        while i < len(intervals):
-            curr = intervals[i]
-            
-            if curr[0] <= newInterval[1] and newInterval[0] <= curr[1]:
-                
-                left = min(newInterval[0], curr[0])
-                
-                while i < len(intervals) and newInterval[1] >= intervals[i][0]:
-                    i+=1
-                    
-                right = max(newInterval[1], intervals[i-1][1])
-                
-                res.append([left, right])
-                break
-            
-            else:
-                res.append(intervals[i])
-                
-                if newInterval[0] > intervals[i][1] and newInterval[1] < intervals[i+1][0]:
-                    res.append(newInterval)
-            
+        while i < n and intervals[i][1] < newInterval[0]:
             i+=1
         
-        # if i < len(intervals)-1:
-        res += intervals[i:]
-                                                                            
-        return res
+        finalIntervals += intervals[:i]
+        
+        # i is possibly at an overlapping interval
+        while i < n and intervals[i][0] <= newInterval[1]:
+            curStart, curEnd = intervals[i]
+            newInterval[0] = min(newInterval[0], curStart)
+            newInterval[1] = max(newInterval[1], curEnd)
+            i+=1
+        
+        finalIntervals.append(newInterval)
+
+        finalIntervals += intervals[i:]
+
+        return finalIntervals
+
+
+
+
+            
+        
+
+
+
+
