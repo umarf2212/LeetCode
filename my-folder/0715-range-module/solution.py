ee5c7dffab01d3173ac51a50a87d161e6d55af16
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-06-02 09:57:25 +0000

    [LeetCode Sync] Runtime - 699 ms (28.02%), Memory - 21.1 MB (96.62%)

--- my-folder/0715-range-module/solution.py
+++ my-folder/0715-range-module/solution.py
@@ -8,48 +8,55 @@ class RangeModule:
         i = 0
         while i < len(self.intervals) and self.intervals[i][1] < left:
             new_intervals.append(self.intervals[i])
-            i += 1
-
+            i+=1
+        
         while i < len(self.intervals) and self.intervals[i][0] <= right:
             left = min(left, self.intervals[i][0])
             right = max(right, self.intervals[i][1])
-            i += 1
-
-        new_intervals.append((left, right))
+            i+=1
+        
+        new_intervals.append([left, right])
 
         while i < len(self.intervals):
             new_intervals.append(self.intervals[i])
-            i += 1
-
+            i+=1
+        
         self.intervals = new_intervals
+        
+
+    def queryRange(self, left: int, right: int) -> bool:
+        lo = 0
+        hi = len(self.intervals)-1
+        while lo <= hi:
+            mid = (lo+hi) // 2
+
+            if self.intervals[mid][0] <= left:
+                lo = mid+1
+            else:
+                hi = mid-1
+        
+        # hi contains the overlapping interval
+        
+        if hi >= 0 and self.intervals[hi][0] <= left and self.intervals[hi][1] >= right:
+            return True
+        return False
+        
 
     def removeRange(self, left: int, right: int) -> None:
         new_intervals = []
+
         for start, end in self.intervals:
             if end <= left or start >= right:
-                new_intervals.append((start, end))
+                new_intervals.append([start, end])
             else:
                 if start < left:
-                    new_intervals.append((start, left))
+                    new_intervals.append([start, left])
+                
                 if end > right:
-                    new_intervals.append((right, end))
-
+                    new_intervals.append([right, end])
+        
         self.intervals = new_intervals
 
-    def queryRange(self, left: int, right: int) -> bool:
-        # Perform binary search to find the rightmost interval with start <= left
-        lo, hi = 0, len(self.intervals)
-        while lo < hi:
-            mid = lo + (hi - lo) // 2
-            if self.intervals[mid][0] <= left:
-                lo = mid + 1
-            else:
-                hi = mid
-
-        # Check if the previous interval covers the given range
-        if lo > 0 and self.intervals[lo - 1][1] >= right:
-            return True
-        return False
 
 # Your RangeModule object will be instantiated and called as such:
 # obj = RangeModule()
