56cdc77d1eab37603c1b89aa403e1eb2e89ef767
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-06-23 13:26:06 +0000

    [LeetCode Sync] Runtime - 213 ms (84.84%), Memory - 20.2 MB (28.80%)

---
+++ my-folder/0966-binary-subarrays-with-sum/solution.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+
+
+        prefixSumCount = defaultdict(int)
+
+        # This is important because think about when there's a subarray
+        # that starts from i=0 to i=n and has prefixSum == goal
+        prefixSumCount[0] = 1
+
+        prefixSum = 0
+        subArrayCount = 0 
+        for i in range(len(nums)):
+            prefixSum += nums[i]
+
+            # if prefixSum - goal in prefixSumCount:
+            subArrayCount += prefixSumCount[prefixSum-goal]
+            
+            prefixSumCount[prefixSum] += 1
+
+        return subArrayCount
