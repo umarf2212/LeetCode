60f810f497cb3d03cc16c1aa181c14fe5bd6d39a
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-07-02 05:36:05 +0000

    [LeetCode Sync] Runtime - 51 ms (40.35%), Memory - 16.6 MB (63.51%)

--- my-folder/0350-intersection-of-two-arrays-ii/solution.py
+++ my-folder/0350-intersection-of-two-arrays-ii/solution.py
@@ -1,16 +1,19 @@
+from collections import defaultdict
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
-        n1 = Counter(nums1)
-        n2 = Counter(nums2)
+        d1 = defaultdict(int)
+        d2 = defaultdict(int)
+
+        for num in nums1:
+            d1[num] += 1
+        
+        for num in nums2:
+            d2[num] += 1
         
         res = []
-        for num, count in n2.items():
-            if num in n1:
-                numCount = min(count, n1[num])
-                res += [num for _ in range(numCount)]
+        for num, count in d1.items():
+            if num in d2:
+                res += [num] * min(count, d2[num])
         
         return res
-        
-        
-        
