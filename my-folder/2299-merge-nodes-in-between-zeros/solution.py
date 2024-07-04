32b06a268ad826b5ee9935800bc2933b7109250e
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-07-04 07:16:16 +0000

    [LeetCode Sync] Runtime - 1022 ms (15.44%), Memory - 89.2 MB (13.35%)

--- my-folder/2299-merge-nodes-in-between-zeros/solution.py
+++ my-folder/2299-merge-nodes-in-between-zeros/solution.py
@@ -6,17 +6,25 @@
 class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-        head = head.next
-        temp = head
-        while temp:
-            currSum = 0
-            curr = temp
-            while temp.val != 0:
-                currSum += temp.val
-                temp = temp.next
-            
-            temp = temp.next
-            curr.val = currSum
-            curr.next = temp
+        if not head:
+            return None
         
-        return head
+        # 0 -> 3 -> 1 -> 0 -> 4 -> 5 -> 2 -> 0
+
+        def merge(curSum, node):
+            if not node: 
+                return
+            
+            if node.val != 0:
+                curSum += node.val
+                return merge(curSum, node.next)
+            
+            else:
+                newNode = ListNode(curSum)
+                newNode.next = merge(0, node.next)
+                return newNode
+        
+        return merge(0, head.next)
+            
+
+
