b76bec26b39d7b375f21138fe6b6f12b1d0eb672
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-07-22 04:49:17 +0000

    [LeetCode Sync] Runtime - 96 ms (88.23%), Memory - 17 MB (63.98%)

--- my-folder/2502-sort-the-people/solution.py
+++ my-folder/2502-sort-the-people/solution.py
@@ -1,8 +1,5 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-
-        ar = [[names[i], heights[i]] for i in range(len(names))]
-
-        ar.sort(key=lambda x:x[1], reverse=True)
-
-        return [name[0] for name in ar]
+        people = list(zip(names, heights))
+        people.sort(key=lambda x:x[1], reverse=True)
+        return [person[0] for person in people]
