fd55ca7bed29b79645ab1a24168f78d66ad301de
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-07-25 05:13:37 +0000

    [LeetCode Sync] Runtime - 1059 ms (58.14%), Memory - 24.8 MB (51.33%)

--- my-folder/0948-sort-an-array/solution.py
+++ my-folder/0948-sort-an-array/solution.py
@@ -1,35 +1,44 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         
-        def MergeSort(ar):
-            if len(ar) == 1:
-                return ar
-                
-            mid = len(ar)//2
-            return merge(MergeSort(ar[:mid]), MergeSort(ar[mid:]))
-        
-        def merge(A, B):
-            if not A: return B
-            if not B: return A
+        # Merge Sort
+
+        # 5 2 | 3 1
+
+        def Merge(ar1, ar2):
+            if not ar1: return ar2
+            if not ar2: return ar1
 
             i = 0
             j = 0
             res = []
-            while i < len(A) and j < len(B):
-                if A[i] <= B[j]:
-                    res.append(A[i])
+            while i < len(ar1) and j < len(ar2):
+                if ar1[i] <= ar2[j]:
+                    res.append(ar1[i])
                     i += 1
                 else:
-                    res.append(B[j])
+                    res.append(ar2[j])
                     j += 1
             
-            if i < len(A):
-                res += A[i:]
+            if i < len(ar1):
+                res += ar1[i:]
             
-            if j < len(B):
-                res += B[j:]
+            if j < len(ar2):
+                res += ar2[j:]
             
             return res
         
-        return MergeSort(nums)
+        def MergeSort(ar):
+            if not ar or len(ar) == 1:
+                return ar
+            
+            mid = len(ar) // 2
 
+            left = MergeSort(ar[:mid])
+            right = MergeSort(ar[mid:])
+
+            return Merge(left, right)
+
+
+
+        return MergeSort(nums)
