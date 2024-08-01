9db23bb25e21b7c751f4c3d472b73ddf91f4ea06
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-08-01 04:08:44 +0000

    [LeetCode Sync] Runtime - 42 ms (82.73%), Memory - 16.6 MB (39.97%)

--- my-folder/2727-number-of-senior-citizens/solution.py
+++ my-folder/2727-number-of-senior-citizens/solution.py
@@ -1,3 +1,11 @@
 class Solution:
     def countSeniors(self, details: List[str]) -> int:
-        return len(list(filter(lambda x:int(x[11:13]) > 60, details)))
+        
+        getPassengerAge = lambda detail: int(detail[11:13])
+
+        count = 0
+        for detail in details:
+            if getPassengerAge(detail) > 60:
+                count+=1
+        
+        return count
