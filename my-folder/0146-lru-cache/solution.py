7352965ed3d1b55bac66289692b0578f9d98638e
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-10-10 11:15:35 +0000

    [LeetCode Sync] Runtime - 623 ms (7.01%), Memory - 78.4 MB (61.09%)

--- my-folder/0146-lru-cache/solution.py
+++ my-folder/0146-lru-cache/solution.py
@@ -1,39 +1,82 @@
-from collections import OrderedDict
+class Node:
+    def __init__(self, key, val):
+        self.val = val
+        self.key = key
+        self.prev = None
+        self.next = None
+
+class DLL:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def insert(self, key, val):
+        newNode = Node(key, val)
+
+        if not self.head:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            newNode.prev = self.tail
+            self.tail = newNode
+        
+        return newNode
+    
+    def delete(self, node):
+        if not node: return
+
+        if not node.prev and not node.next:
+            self.head = None
+
+        elif node == self.head:
+            self.head = self.head.next
+            self.head.prev = None
+
+        elif node == self.tail:
+            node.prev.next = None
+            self.tail = node.prev
+
+        elif node.next and node.prev:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+
+        return node
+
 class LRUCache:
 
-    def __init__(self, capacity: int):
+    # @param capacity, an integer
+    def __init__(self, capacity):
         self.capacity = capacity
         self.size = 0
-        self.d = OrderedDict()
+        self.dll = DLL()
+        self.cache = {}
 
-    def get(self, key: int) -> int:
-        if key not in self.d:
-            return -1
+    # @return an integer
+    def get(self, key):
+        if key in self.cache:
+            self.dll.delete(self.cache[key])
+            self.cache[key] = self.dll.insert(key, self.cache[key].val)
+            return self.cache[key].val
+        return -1
 
-        temp = self.d[key]
-        del self.d[key]
-        self.d[key] = temp
-        # print('GET ->', self.d)
-        return temp
-        
-    def put(self, key: int, value: int) -> None:
-        
-        if key in self.d:
-            del self.d[key]
-            self.size -= 1
+    # @param key, an integer
+    # @param value, an integer
+    # @return nothing
+    def put(self, key, value):
+        if key in self.cache:
+            self.dll.delete(self.cache[key])
+            self.cache[key] = self.dll.insert(key, value)
+            return 
 
         if self.size == self.capacity:
-            del self.d[next(iter(self.d))]
+            del self.cache[self.dll.head.key]
+            self.dll.delete(self.dll.head)
             self.size -= 1
-
         
-        self.d[key] = value
+        self.cache[key] = self.dll.insert(key, value)
         self.size += 1
+        
+        
 
-        # print('PUT ->', self.d)
 
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
