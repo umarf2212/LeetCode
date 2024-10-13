e75e9d9b0e97604298bd4754e07e35eb1808115e
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-10-13 11:54:38 +0000

    [LeetCode Sync] Runtime - 52 ms (5.04%), Memory - 16.7 MB (100.00%)

--- my-folder/1036-rotting-oranges/solution.py
+++ my-folder/1036-rotting-oranges/solution.py
@@ -1,54 +1,51 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        
-        
-        m, n = len(grid), len(grid[0])
-        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-        dist = [[float('inf') for _ in range(n)] for _ in range(m)]
-        vis = [[False for _ in range(n)] for _ in range(m)]
+        # Multi-source BFS from each rotten orange to 
+        # find shortest distance to each fresh orange.
+        # The max distance to any fresh orange is the 
+        # min time it takes to rot all oranges.
+
+        m, n = len(grid), len(grid[0])
+        inf = float('inf')
+
+        dist = [[inf for _ in range(n)] for _ in range(m)]
 
         q = deque()
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
-                    dist[i][j] = 0
                     q.append((i, j, 0))
-                    vis[i][j] = True
-            
-        def isValid(i, j):
-            if i<0 or i>=m or j<0 or j>=n or grid[i][j]==0 or grid[i][j]==2 or vis[i][j]:
-                return False
-            return True
+                    dist[i][j] = 0
         
-
+        dirs = [(-1,0),(1,0),(0,-1),(0,1)]
         while q:
             i, j, d = q.popleft()
 
-            # dist[i][j] = d
-
             for dx, dy in dirs:
-                new_i = dx + i
-                new_j = dy + j
+                nI = dx + i
+                nJ = dy + j
 
-                if isValid(new_i, new_j):
-                    new_d = min(d+1, dist[new_i][new_j])
-                    vis[new_i][new_j] = True
-                    dist[new_i][new_j] = new_d
-                    q.append((new_i, new_j, new_d))
+                if 0 <= nI < m and 0 <= nJ < n:
+                    if grid[nI][nJ] == 1:
+                        newDist = d+1
+
+                        if newDist < dist[nI][nJ]:
+                            dist[nI][nJ] = newDist
+                            q.append((nI, nJ, newDist))
+                            grid[nI][nJ] = 2
         
-        ans = -1
-        allRotten = True
+        maxDist = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    allRotten = False
-
-                if grid[i][j] == 1:
-                    ans = max(ans, dist[i][j])
+                    return -1
+                
+                if dist[i][j] != inf:
+                    maxDist = max(maxDist, dist[i][j])
         
-        if allRotten: return 0
-        
-        return ans if ans != float('inf') else -1
+        return maxDist
 
+
+        
