7a32dffbb1c88d0f9ceba5fe6de330b85725f3cf
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-10-16 08:44:52 +0000

    [LeetCode Sync] Runtime - 316 ms (7.54%), Memory - 18.1 MB (100.00%)

--- my-folder/0329-longest-increasing-path-in-a-matrix/solution.py
+++ my-folder/0329-longest-increasing-path-in-a-matrix/solution.py
@@ -1,39 +1,42 @@
-from collections import defaultdict
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+
+        m, n = len(matrix), len(matrix[0])
         
-        # build graph
-        # do DFS on the graph with max path
-
-        A = matrix
-        m, n = len(A), len(A[0])
-
         def isValid(i, j):
-            if i<0 or i>=m or j<0 or j>=n:
+            if (i < 0 or i >= m) or (j < 0 or j >= n):
                 return False
             return True
-        
-        dx = [-1, 1, 0, 0]
-        dy = [0, 0, -1, 1]
-        
-        memo = [[1 for _ in range(n)] for _ in range(m)]
+
+        memo = [[-1]*n for _ in range(m)]
+
         def dfs(i, j):
-            if memo[i][j] > 1:
+            if not isValid(i, j):
+                return 0
+            
+            if memo[i][j] != -1:
                 return memo[i][j]
-
-            for k in range(4):
-                    x = i + dx[k]
-                    y = j + dy[k]
-
-                    if isValid(x, y) and A[x][y] > A[i][j]:
-                        memo[i][j] = max(memo[i][j], 1 + dfs(x, y))
-
-            return memo[i][j]
+                
+            ans = 0
+            if isValid(i-1, j) and matrix[i-1][j] > matrix[i][j]:
+                ans = max(ans, dfs(i-1, j))
+            
+            if isValid(i+1, j) and matrix[i+1][j] > matrix[i][j]:
+                ans = max(ans, dfs(i+1, j))
+            
+            if isValid(i, j-1) and matrix[i][j-1] > matrix[i][j]:
+                ans = max(ans, dfs(i, j-1))
+            
+            if isValid(i, j+1) and matrix[i][j+1] > matrix[i][j]:
+                ans = max(ans, dfs(i, j+1))
+            
+            memo[i][j] = ans + 1
+            
+            return ans + 1
         
-        ans = -1
+        ans = 0
         for i in range(m):
             for j in range(n):
-                ans = max(ans, dfs(i, j) )
-        
+                ans = max(ans, dfs(i, j))
+            
         return ans
-
