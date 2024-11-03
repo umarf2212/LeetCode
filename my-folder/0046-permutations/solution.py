da81cafdb87afbb7fd268855919489c2bbb0c0f6
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2024-11-03 11:44:57 +0000

    [LeetCode Sync] Runtime - 1 ms (49.29%), Memory - 16.7 MB (100.00%)

--- my-folder/0046-permutations/solution.py
+++ my-folder/0046-permutations/solution.py
@@ -1,3 +1,27 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return permutations(nums)
+        
+        def backtrack(nums, S, ar, result):
+
+            idx = len(S)
+
+            if idx == len(nums):
+                result.append(ar[:])
+                return
+
+            for i in range(len(nums)):
+                if nums[i] not in S:
+                    ar[idx] = nums[i]
+                    S.add(nums[i])
+                    backtrack(nums, S, ar, result)
+                    S.remove(nums[i])
+
+                    
+
+        S = set()
+        ar = [0] * len(nums)
+        result = []
+        backtrack(nums, S, ar, result)
+        return result
+
+
