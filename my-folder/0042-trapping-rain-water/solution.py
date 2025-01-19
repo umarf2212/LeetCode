3d9322952dded89debc89d8b6b6e7d043daca70c
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2025-01-19 18:56:02 +0000

    [LeetCode Sync] Runtime - 27 ms (30.56%), Memory - 19.3 MB (38.42%)

--- my-folder/0042-trapping-rain-water/solution.py
+++ my-folder/0042-trapping-rain-water/solution.py
@@ -7,17 +7,14 @@ class Solution:
             maxSoFar = max(maxSoFar, height[i])
             maxLeft.append(maxSoFar)
         
-        maxRight = []
         maxSoFar = -1
         for i in range(len(height)-1, -1, -1):
             maxSoFar = max(maxSoFar, height[i])
-            maxRight.append(maxSoFar)
-        maxRight = maxRight[::-1]
+            maxLeft[i] = min(maxLeft[i], maxSoFar)
         
         ans = 0
         for i in range(len(height)):
-            minHeight = min(maxLeft[i], maxRight[i])
-            ans += minHeight - height[i]
+            ans += maxLeft[i] - height[i]
         
         return ans
 
