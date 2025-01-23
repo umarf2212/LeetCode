2a0674ea02ac4c12d6eb1e85cfc9768243c9522c
Umar Farooque <36859307+umarf2212@users.noreply.github.com>
2025-01-23 07:14:09 +0000

    [LeetCode Sync] Runtime - 198 ms (31.05%), Memory - 20.8 MB (44.36%)

--- my-folder/0542-01-matrix/solution.py
+++ my-folder/0542-01-matrix/solution.py
@@ -1,44 +1,33 @@
 from collections import deque
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-
+        
         q = deque()
 
         m, n = len(mat), len(mat[0])
 
-        dist = [[float('inf') for _ in range(n)] for _ in range(m)]
+        dist = [[float('inf')] * n for _ in range(m)] 
         for i in range(m):
             for j in range(n):
                 if mat[i][j] == 0:
                     dist[i][j] = 0
                     q.append( (i, j, 0) )
         
-        def isValid(i, j):
+        def is_valid(i, j):
             if 0 <= i < m and 0 <= j < n and mat[i][j] == 1 and dist[i][j] == float('inf'):
                 return True
             return False
-
-    
+        
         while q:
             i, j, d = q.popleft()
 
-            for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                ix = i + x
-                jy = j + y
+            for x, y in [(-1,0), (1,0), (0,-1), (0,1)]:
+                new_i = i + x
+                new_j = j + y
 
-                if isValid(ix, jy):
+                if is_valid(new_i, new_j):
                     new_dist = d+1
-                    dist[ix][jy] = min( dist[ix][jy], new_dist )
-                    q.append((ix, jy, new_dist))
+                    dist[new_i][new_j] = min(dist[new_i][new_j], new_dist)
+                    q.append( (new_i, new_j, new_dist) )
         
         return dist
-
-
-
-        # 0 0 0 
-        # 1 0 1
-        # 1 1 1 
-
-        # For every 0 cell, the dist is 0
-        # Start BFS from every 0 cell and when 1 cell is reached,
-        # update its distance with min
